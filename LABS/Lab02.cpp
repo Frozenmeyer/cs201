d0f1f4f0d708a1939ffe99b28936b44ac9c782f3
Leif King <loking@alaska.edu>
2021-09-08 09:35:39 -0800

    Added defunct LAB files that currently don't work (undefined reference 'WinMain')

---
+++ LABS/Lab02.cpp
@@ -0,0 +1,9 @@
+// Homework0 
+// Leif King
+// 8/30/2021
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hellow World!" << std::endl;
+}
---
+++ LABS/Lab03.cpp
@@ -0,0 +1,15 @@
+// Lab 02 
+// Leif King
+// 9/3/2021
+//
+// Description: 1 Write a C++ program that prints the numbers 1 to 20, using a while-loop.
+//              2 Write a C++ program that prints a 60 × 10 box made of some single character, using nested while-loops. For example, here is an 8 × 3 box made of asterisks.
+//********
+//********
+//********
+
+#include <iostream>
+
+int main(){
+    
+}
