a13aa689f89e87eb5229f9cae0b1b268a2c6593f
Leif King <loking@alaska.edu>
2021-09-08 09:43:59 -0800

    Updated Lab02.ccp since things are now working

--- LABS/Lab02.cpp
+++ LABS/Lab02.cpp
@@ -3,7 +3,14 @@
 // 8/30/2021
 
 #include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main(){
-    std::cout << "Hellow World!" << std::endl;
+    std::cout << "Type A number" << std::endl;
+    int number;
+    std::cin >> number;
+    std::cout << "You typed: " << number << std::endl;
+
 }
