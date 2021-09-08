ddfc3674be2a6cf032470ece54ac816456ba8d2c
Leif King <loking@alaska.edu>
2021-09-08 09:55:56 -0800

    Working on Lab02.cpp

--- LABS/Lab02.cpp
+++ LABS/Lab02.cpp
@@ -8,9 +8,8 @@ using std::cin;
 using std::endl;
 
 int main(){
-    std::cout << "Type A number" << std::endl;
+    std::cout << "Type A number, I'll add one to it and square it" << std::endl;
     int number;
     std::cin >> number;
-    std::cout << "You typed: " << number << std::endl;
-
+    std::cout << "You typed: " << ((number+1)^2) << std::endl;
 }
